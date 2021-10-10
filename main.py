62dd735bc92ba365cb56fb5ad89e43c8b7d7a37b
Raul Colina <rcolina@gmail.com>
2021-10-10 12:27:51 -0700

    GuessGame Corregido

--- main.py
+++ main.py
@@ -18,4 +18,33 @@ def humanguess():
             print(f"El numero {number} es menor")
 
     humanguess()
-        
+
+def computerGuess():
+    #Escogi el 500
+    firstNumber=0
+    secondNumber=1000
+    number=random.randrange(firstNumber,secondNumber)
+    bandera=False
+    
+    
+    while(bandera==False):
+        print (number)
+        print ("Este es tu numero?")
+        opc=input("si | no: ").lower()
+        if (opc =="no"):
+            print("Tu numero es mayor o menor al mio?")
+            opc2=input("mayor | menor: ").lower()
+            if(opc2=="menor"):
+                secondNumber=number
+                newNumber=random.randrange(firstNumber,secondNumber)#el de la computadora es mayor
+                number = newNumber
+            
+            else:#en caso de que sea mayor
+                firstNumber=number
+                newNumber=random.randrange(firstNumber,secondNumber)
+                number=newNumber
+        else:
+            bandera=True
+            print('Ganaste')
+computerGuess()
+
